eff0e65fedf4b95bf15c4b2b3091754554cdcc48
PuwitS <puwit33013@gmail.com>
2024-09-01 20:38:00 +0700

    Add movement and fix update the event loop location

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -15,12 +14,12 @@ def main():
             if event.type == pygame.QUIT:
                 return      
               
-            player1.update(dt)                
-            screen.fill("black")
-            player1.draw(screen)
+        player1.update(dt)                
+        screen.fill("black")
+        player1.draw(screen)
 
-            pygame.display.flip()
-            dt = clock.tick(60)/1000
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
             
 
 if __name__ == "__main__":
